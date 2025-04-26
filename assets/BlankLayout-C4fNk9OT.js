c03f844e13275e2c995551fec3db466c4e7ef4ae
AndresfCrialesc <afcrialesc@gmail.com>
2025-04-25 21:28:50 -0500

    Updates

--- assets/BlankLayout-C4fNk9OT.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as s}from"./index-CUdZfMXs.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx(s,{})});export{e as default};
--- assets/PrivateRoute-upjKY0H9.js
+++
@@ -1,1 +0,0 @@
-import{h as a,j as t,k as s,O as u}from"./index-CUdZfMXs.js";const n=()=>{const{token:e,loading:r}=a();return r?null:e?t.jsx(u,{}):t.jsx(s,{to:"/auth/login",replace:!0})};export{n as default};
