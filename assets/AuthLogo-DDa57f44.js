44d05995cfb439b5d45220a0126dbed1f583811c
AndresF_CrialesC <afcrialesc@gmail.com>
2025-06-13 17:19:01 -0500

    Updates

---
+++ assets/AuthLogo-DDa57f44.js
@@ -0,0 +1,1 @@
+import{j as t,L as o}from"./index-D6nLFQig.js";import{s}from"./Box-BvN1tnWp.js";const e="/assets/booth_logo-Df5-8_K4.png",i=s(o)(()=>({height:"64px",width:"180px",overflow:"hidden",display:"block"})),r=()=>t.jsx(i,{to:"/",style:{display:"flex",alignItems:"center"},children:t.jsx("img",{src:e,alt:"logo",style:{width:"174px",height:"64px"}})});export{r as A,e as L};
