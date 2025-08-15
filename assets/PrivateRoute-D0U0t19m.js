9aa4c2efb5c5532d09dd7d582c950f77a79507f3
AndresF_CrialesC <afcrialesc@gmail.com>
2025-08-15 17:36:26 -0500

    Updates

---
+++ assets/PrivateRoute-D0U0t19m.js
@@ -0,0 +1,1 @@
+import{h as a,j as t,m as s,O as u}from"./index-C2jMbD_L.js";const n=()=>{const{token:e,loading:r}=a();return r?null:e?t.jsx(u,{}):t.jsx(s,{to:"/auth/login",replace:!0})};export{n as default};
