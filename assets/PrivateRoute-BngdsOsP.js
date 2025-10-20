26b26f2883f1eed7003c59ae1b6699819ec68ec9
AndresF_CrialesC <afcrialesc@gmail.com>
2025-10-20 15:12:10 -0500

    Updates

--- assets/PrivateRoute-BngdsOsP.js
+++
@@ -1,1 +0,0 @@
-import{h as a,j as t,o,O as s}from"./index-DH1NjL0p.js";const n=()=>{const{token:e,loading:r}=a();return r?null:e?t.jsx(s,{}):t.jsx(o,{to:"/auth/login",replace:!0})};export{n as default};
--- assets/isMuiElement-B5V1_x0p.js
+++
@@ -1,1 +0,0 @@
-import{r as m}from"./index-DH1NjL0p.js";function p(i,e){var a,r,t;return m.isValidElement(i)&&e.indexOf(i.type.muiName??((t=(r=(a=i.type)==null?void 0:a._payload)==null?void 0:r.value)==null?void 0:t.muiName))!==-1}export{p as i};
