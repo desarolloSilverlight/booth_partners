5226ddd89f0e2c9e13cbc658e38f135ac1c50eba
AndresfCrialesc <afcrialesc@gmail.com>
2025-10-27 22:48:50 -0500

    Updates

--- src/config/config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-    rutaApi: import.meta.env.VITE_API_URL,
-    rutaApiBuk: import.meta.env.VITE_API_BUK_URL,
-    tokenApiBuk: import.meta.env.VITE_TOKEN_API_BUK
-};
-
-export default config;
