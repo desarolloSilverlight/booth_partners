c821f2fd695769a4ae010819a2c4dcccdbe762a7
AndresfCrialesc <afcrialesc@gmail.com>
2025-10-31 17:48:11 -0500

    Updates

--- assets/PrivateRoute-CCTgO9wN.js
+++
@@ -1,1 +0,0 @@
-import{h as a,j as t,n as s,O as u}from"./index-DArN1dFc.js";const o=()=>{const{token:e,loading:r}=a();return r?null:e?t.jsx(u,{}):t.jsx(s,{to:"/auth/login",replace:!0})};export{o as default};
