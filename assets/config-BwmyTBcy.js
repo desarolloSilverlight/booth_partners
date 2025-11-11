5749b3f01517985743fd60e135d5da42c887f8e4
AndresF_CrialesC <afcrialesc@gmail.com>
2025-11-11 17:12:02 -0500

    Updates

--- assets/config-BwmyTBcy.js
+++
@@ -1,1 +0,0 @@
-const t={rutaApi:"https://application.boothandpartners.co/",rutaApiBuk:"https://boothandpartners.buk.co/api/v1/colombia/",tokenApiBuk:"nLxqha5dsBxzpFMnrFX2XaP2"};export{t as c};
---
+++ assets/config-DOW8WNQj.js
@@ -0,0 +1,1 @@
+const o={rutaApi:"https://application.boothandpartners.co/",rutaApiBuk:"https://boothandpartners.buk.co/api/v1/colombia/",tokenApiBuk:"nLxqha5dsBxzpFMnrFX2XaP2",profile:0};export{o as c};
